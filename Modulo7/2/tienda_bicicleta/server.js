3d38fbb5fe05d64b10d8e0476f38dd2df1696c35
Alejandro Lagos <alejandro.lagosjara@gmail.com>
2026-05-13 22:06:06 -0400

    Agregar proyecto tienda_bicicletas y configuracion db

---
+++ Modulo7/2/tienda_bicicleta/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const methodoverride = require('method-override')
+const PORT = process.env.PORT  || 3000
+
+const bicicletasRouter = require ('.src/routes/bicicletas')
+
+const app = express()
+
+app.use(express.urlencoded( {extended:true}))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(methodoverride('_method'))
+
+app.engine('handlebars', exphbs.engine ({
+    defaultLayout: 'main',
+    partialsDir: path.join(__dirname, 'src', 'views', 'parcials')
+}))
+app.set('view engine', 'hadlebars')
+app.set('views', path.join(__dirname, 'src', 'views'))
+
+app.use('/bicicletas', bicicletasRouter)
+
+app.get('/', (req, res) => res.redirect('/bicicletas'))
+
+app.listen(PORT, () => {
+    console.log(`Servidor inicializado en http://localhost:${PORT}`)
+})
