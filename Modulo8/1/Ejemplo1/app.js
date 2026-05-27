d9539bd1ac878a9ceec348e298ec8cba910650cd
Alejandro Lagos <alejandro.lagosjara@gmail.com>
2026-05-27 19:09:34 -0400

    Metodo POST y arquitectura Rest

---
+++ Modulo8/1/Ejemplo1/app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const fs = require('fs') //file system
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+const leePeliculas = () => {
+    const data = fs.readFileSync('peliculas.json','utf8')
+    return JSON.parse(data)
+}
+
+const guardarPelicula = (pelicula) => {
+    fs.writeFileSync('peliculas.json', JSON.stringify(pelicula, null, 4))
+}
+
+app.get('/api/v1/peliculas', (req, res) => {
+    const peliculas = leePeliculas()
+    res.json(peliculas)
+})
+
+app.get('/api/v1/peliculas/:id', (req, res) => {
+    const peliculas = leePeliculas()
+    const pelicula = peliculas.find(p => p.id === parseInt(req.params.id))
+    if(!pelicula) return res.status(404).json({error: "Pelicula no encontrada"})
+
+    res.json(pelicula)
+})
+
+app.post('/api/v1/peliculas', (req,res) => {
+    const peliculas = leePeliculas()
+    const { titulo, direccion, protagonista, genero, estreno } = req.body
+    // sigue validacion para que nos llegue todos los datos
+    if (!titulo || !direccion || !protagonista || !genero || !estreno) return res.status(400).json({error: "Todos los datos son obligatorios"})
+    
+    const nuevaPelicula = {
+        id: peliculas.length ? peliculas[peliculas.length -1 ].id + 1 : 1 ,
+        titulo,
+        direccion, 
+        protagonista,
+        genero, 
+        estreno
+    }
+
+    peliculas.push(nuevaPelicula)
+    guardarPelicula(peliculas)
+
+    res.status(201).json({mensaje: "Pelicula Creada", pelicula:nuevaPelicula})
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor inicializado en http://localhost:${PORT}`)
+})
